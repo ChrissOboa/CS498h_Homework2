7e83b1c69a16087b44cb4d9d25064b4b32938860
ChrissOboa <90800696+ChrissOboa@users.noreply.github.com>
2021-12-13 14:12:30 -0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,35 @@
+#Chriss Jordan Oboa
+#Professor Liu
+#CS498H
+#December 10th
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers 
+
+(train_images, train_labels), (test_images, test_labels) = keras.datasets.mnist.load_data()
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+train_labels = tf.keras.utils.to_categorical(train_labels)
+test_labels = tf.keras.utils.to_categorical(test_labels)
+
+model = keras.Sequential([
+  keras.layers.InputLayer(input_shape=(28, 28)),
+  keras.layers.Reshape(target_shape=(28, 28, 1)),
+  keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation=tf.nn.relu),
+  keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation=tf.nn.relu),
+  keras.layers.MaxPooling2D(pool_size=(2, 2)),
+  keras.layers.Dropout(0.25),
+  keras.layers.Flatten(),
+  keras.layers.Dense(10)
+])
+
+model.compile(optimizer= 'adam' , loss= keras.losses.binary_crossentropy, metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print('Test Accuracy:', test_acc)
